25cd46aa84187ada2f0e26a56c1e75dbe9bff152
chspower1 <chspower1@gmail.com>
2022-07-23 21:38:45 +0900

    Add TO Do List

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 function App() {
+    const [toDo, setToDo] = useState();
+    const [toDos, setToDos] = useState([]);
+    const onChange = (e) => setToDo(e.target.value);
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!toDo) return;
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    };
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!</h1>
-            <Button text={"Hi"} />
+            <h1>My To Dos({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    onChange={onChange}
+                    type="text"
+                    value={toDo}
+                    placeholder="Write your to do"
+                />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
         </div>
     );
 }
