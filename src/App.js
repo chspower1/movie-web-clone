c4b5c65ff55f26756163855475a0e7bd3b8b52a1
chspower1 <chspower1@gmail.com>
2022-07-23 23:49:07 +0900

    bitcoin exchange

--- src/App.js
+++ src/App.js
@@ -4,6 +4,30 @@ import styles from "./App.module.css";
 function App() {
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
+    const [input, setInput] = useState();
+    const [selectCoin, setSelectCoin] = useState();
+    const [inverter, setInverter] = useState(false);
+
+    const onChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    // const onClick = (e) => {};
+
+    const selectedCoin = (e) => {
+        setSelectCoin(e.target.value.split(" ").at(-2));
+        console.log(e);
+    };
+    const inverted = (e) => {
+        setInverter((current) => !current);
+        setInput(inverter ? input * selectCoin : input / selectCoin);
+        console.log(input);
+        console.log(selectCoin);
+    };
+
+    const reset = () => {
+        setInput(0);
+    };
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers") //
             .then((res) => res.json())
@@ -16,14 +40,50 @@ function App() {
     return (
         <div>
             <h1>The Coins!</h1>
-            {loading ? <div>loading...</div> : null}
-            <ul>
-                {coins.map((coin) => (
-                    <li>
-                        {coin.name}({coin.symbol}) : {coin.quotes.USD.price.toFixed(2)}$
-                    </li>
-                ))}
-            </ul>
+            {loading ? (
+                <div>loading...</div>
+            ) : (
+                <div>
+                    <select onChange={selectedCoin}>
+                        <option>Select Coins to exchange!</option>
+                        {coins.map((coin) => (
+                            <option>
+                                {coin.name}({coin.symbol}) : {coin.quotes.USD.price.toFixed(2)} $
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            )}
+            <div>
+                <form>
+                    <div>
+                        <label for="money">Your Money</label>
+                        <input
+                            onChange={onChange}
+                            id="money"
+                            type="number"
+                            value={inverter ? input * selectCoin : input}
+                            placeholder="money(dollar)"
+                            disabled={inverter}
+                        />
+                    </div>
+                    <div>
+                        <label for="bitcoin">Coin</label>
+                        <input
+                            onChange={onChange}
+                            id="bitcoin"
+                            type="number"
+                            value={inverter ? input : input / selectCoin}
+                            placeholder="bitcoin"
+                            disabled={!inverter}
+                        />
+                    </div>
+                </form>
+                <button onClick={inverted}>
+                    {!inverter ? "money->bitcoin" : "bitcoin->money"}
+                </button>
+                <button onClick={reset}>reset</button>
+            </div>
         </div>
     );
 }
