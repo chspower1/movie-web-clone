9fab3d83842afa2c801c6ec400c8c6330af64abf
chspower1 <chspower1@gmail.com>
2022-07-24 17:40:08 +0900

    Fixed useEffect Error

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -14,7 +14,9 @@ function Detail() {
     };
     console.log({ movie }); //for checking
 
-    useEffect(() => getMovie(), []);
+    useEffect(() => {
+        getMovie();
+    }, []);
 
     return (
         <div>
